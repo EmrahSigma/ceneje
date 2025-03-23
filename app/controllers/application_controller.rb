652266ccc660cf0d6a2e88b59d4d403170625793
EmrahSigma <emrah.sekic@scv.si>
2025-03-23 21:16:44 +0100

    masala

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  def index
+    # Fetch all the products you want to display on the homepage
+    @products = Product.all
+  end
 end
